4df7dba3fb76929e23171b66cbcf586b45d5f555
wilbertsky <willscharff@icloud.com>
2016-12-30 12:41:36 -0800

    Open_List -Adding more files to the slim framework setup.

--- public/index.php
+++ public/index.php
@@ -2,13 +2,85 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+//Adding composer classes:
 require '../vendor/autoload.php';
+//Our own classes under classes directory:
+spl_autoload_register(function ($classname) {
+    require ("../classes/" . $classname . ".php");
+});
+//Config file for db connections: Instructions here: https://www.slimframework.com/docs/tutorial/first-app.html#add-config-settings-to-your-application
+require '../config/config.php';
 
-$app = new \Slim\App;
+$app = new \Slim\App(["settings" => $config]);
+$container = $app->getContainer();
+//Setting up logging:
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+//Setting up DB container:
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+//Setting up views:
+$container['view'] = new \Slim\Views\PhpRenderer("../templates/");
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
 
     return $response;
 });
+
 $app->run();
+//Routes examples:
+/*
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->get('/tickets', function (Request $request, Response $response) {
+    $this->logger->addInfo("Ticket list");
+    $mapper = new TicketMapper($this->db);
+    $tickets = $mapper->getTickets();
+
+    $response->getBody()->write(var_export($tickets, true));
+    return $response;
+});
+
+$app->post('/ticket/new', function (Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    $ticket_data = [];
+    $ticket_data['title'] = filter_var($data['title'], FILTER_SANITIZE_STRING);
+    $ticket_data['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
+
+$app->get('/ticket/{id}', function (Request $request, Response $response, $args) {
+    $ticket_id = (int)$args['id'];
+    $mapper = new TicketMapper($this->db);
+    $ticket = $mapper->getTicketById($ticket_id);
+
+    $response->getBody()->write(var_export($ticket, true));
+    return $response;
+});
+//Example with view:
+$app->get('/tickets', function (Request $request, Response $response) {
+    $this->logger->addInfo("Ticket list");
+    $mapper = new TicketMapper($this->db);
+    $tickets = $mapper->getTickets();
+
+    $response = $this->view->render($response, "tickets.phtml", ["tickets" => $tickets]);
+    return $response;
+});
+*/
+
+
