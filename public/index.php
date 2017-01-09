5db6b930713b0915c0a8a9f0d99d69ec0bffeefc
wilbertsky <willscharff@icloud.com>
2017-01-08 17:08:17 -0800

    Cleaned up index.php routes file.

--- public/index.php
+++ public/index.php
@@ -32,16 +32,6 @@ $container['db'] = function ($c) {
 //Setting up views:
 $container['view'] = new \Slim\Views\PhpRenderer("../templates/");
 
-//Create a route for the home page
-$app->get('/yomama',function (Request $request, Response $response){
-	$this->logger->addInfo("Home");
-	$mapper =  new HomeMapper($this->db);
-	$home = $mapper->getHome();
-	//$response = $this->view->render($response, "home.php");
-	$response = $this->view->render($response, "home.php", ["home" => $home, "router" => $this->router]);
-    return $response;
-});
-
 //***************** API ***************** 
 //Create a route for the API Events 
 $app->get('/api/events',function (Request $request, Response $response){
@@ -52,70 +42,6 @@ $app->get('/api/events',function (Request $request, Response $response){
     return $response;
 });
 
-
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
-});
-
 $app->run();
-
-//Working on events API and response:
-/*
-$app->post('/events', function (Request $request, Response $response) {
-    $this->logger->addInfo("Event list");
-    $mapper = new events(this->db);
-    $events = $mapper->getEvents();
-
-    $response = $this->view->render($response, "events.php", ["events" => $events]);
-    return $response;
-});
-*/
-
-
-//Routes examples:
-/*
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
-});
-
-$app->get('/tickets', function (Request $request, Response $response) {
-    $this->logger->addInfo("Ticket list");
-    $mapper = new TicketMapper($this->db);
-    $tickets = $mapper->getTickets();
-
-    $response->getBody()->write(var_export($tickets, true));
-    return $response;
-});
-
-$app->post('/ticket/new', function (Request $request, Response $response) {
-    $data = $request->getParsedBody();
-    $ticket_data = [];
-    $ticket_data['title'] = filter_var($data['title'], FILTER_SANITIZE_STRING);
-    $ticket_data['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
-
-$app->get('/ticket/{id}', function (Request $request, Response $response, $args) {
-    $ticket_id = (int)$args['id'];
-    $mapper = new TicketMapper($this->db);
-    $ticket = $mapper->getTicketById($ticket_id);
-
-    $response->getBody()->write(var_export($ticket, true));
-    return $response;
-});
-//Example with view:
-$app->get('/tickets', function (Request $request, Response $response) {
-    $this->logger->addInfo("Ticket list");
-    $mapper = new TicketMapper($this->db);
-    $tickets = $mapper->getTickets();
-
-    $response = $this->view->render($response, "tickets.phtml", ["tickets" => $tickets]);
-    return $response;
-});
-*/
 
 
